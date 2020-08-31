c15f13a8cb517110fb02428831516c85db8f3553
Kaustubh_Shrivastava <ishugrga@gmail.com>
2020-08-31 16:46:37 +0530

    added soft and iterative approach of binary search

--- searching/LinearSearch.cpp
+++ searching/LinearSearch.cpp
@@ -1,8 +1,41 @@
+//  this is a programe implimenting the Linear search...
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int linearSearch(vector<int> vec, int find)
+{
+}
+
+void show(vector<int> vec)
+{
+  cout << "\n";
+  for (auto it = vec.begin(); it <= vec.end(); it++)
+    cout << *it << " ";
+}
+
 int main()
 {
-  cout << "hello world ";
+  vector<int> v;
+  cout << "Enter the size of the array you are inserting :";
+  int size;
+  cin >> size;
+
+  cout << "\nEnter an Unsorted array";
+
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cin >> n;
+    v.push_back(n);
+  }
+
+  show(v);
+  cout << "\n Enter an item to be searched :";
+  int find;
+  cin >> find;
+
+  linearSearch(v, find);
+
   return 0;
 }
